8b85c4b46f3455c0e25add8ba64ab456834ad011
rodrigo vela <rodrigoandre777@gmail.com>
2023-10-31 22:41:11 -0400

    feat(dialog, #5): adding dialog component to request clients order

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
+import { Optional } from '@interfaces/optional/optional';
 import { Product } from '@interfaces/product/product';
+import { OptionalService } from '@services/optional/optional.service';
 import { ProductsService } from '@services/products/products.service';
 import { DynamicDialogConfig } from 'primeng/dynamicdialog';
-
-
 
 @Component({
   selector: 'app-dialog',
@@ -12,18 +12,26 @@ import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 })
 export class DialogComponent {
   amount = 1;
+  backgroundColor = '';
   product: Product = {} as Product;
+  optional: Optional[] = [];
 
-  constructor(public config: DynamicDialogConfig, private productService: ProductsService){
+  constructor(public config: DynamicDialogConfig, private productService: ProductsService, private optionalService: OptionalService){
     this.getProductById();
+    this.getAllOptional();
+    console.log(this.optional);
+    this.backgroundColor =  this.config.data.color;
   }
 
   getProductById(){
     this.productService.getById(this.config.data.id).subscribe((product: Product) => {
       this.product = product;
-    })
+    });
   }
 
-  
-
+  getAllOptional(){
+    this.optionalService.getAllOptional().subscribe((optional: Optional[]) => {
+      this.optional = optional;
+    });
+  }
 }
